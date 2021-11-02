f368c9fe041bcddb3453226d8b65ab7444e7b816
huydang208 <huydang191@gmail.com>
2021-11-03 04:07:47 +0700

    Generate navbar & login component, routing login - HuyDA

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Route } from './commons/enums/route.enum';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
       import('./feature-modules/home/home.module').then((m) => m.HomeModule),
-  }
+  },
+  {
+    path: Route.LOGIN,
+    loadChildren: () =>
+      import('./feature-modules/login/login.module').then((m) => m.LoginModule),
+  },
 ];
 
 @NgModule({
--- src/app/feature-modules/home/home.module.ts
+++ src/app/feature-modules/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { NavbarModule } from 'src/app/commons/shared-modules/navbar/navbar.module';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { HomeComponent } from './home.component';
   imports: [
     CommonModule,
     HomeRoutingModule,
+    NavbarModule
   ]
 })
 export class HomeModule { }
