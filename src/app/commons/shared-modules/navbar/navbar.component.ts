e32c978a48cdae921320469fb6b85fdf6b1121bc
huydang208 <huydang191@gmail.com>
2021-11-05 03:52:15 +0700

    update navbar UI - HuyDA

--- src/app/commons/shared-modules/navbar/navbar.component.ts
+++ src/app/commons/shared-modules/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart, Event as NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   public defaultUser: string = 'ThinhMD';
+  showBanner?: boolean;
 
   constructor(private readonly router: Router) {}
+
+  ngOnInit(): void{
+    this.displayBanner();
+  }
 
   toProfile() {
     this.router.navigate(['profile', this.defaultUser]);
@@ -17,6 +22,13 @@ export class NavbarComponent {
 
   toLogout() {
     console.log('logout');
+  }
 
+  displayBanner(){
+    this.router.events.subscribe((event: NavigationEnd) => {
+      if(event instanceof NavigationStart){
+        this.showBanner = event.url === '/' ? true : false;
+      }
+    })
   }
 }
--- src/app/feature-modules/profile/user-profile/user-profile.component.ts
+++ src/app/feature-modules/profile/user-profile/user-profile.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
 })
 export class UserProfileComponent {
   defaultUser: string = "ThinhMD";
-  defaultBio: string = "Mô tả..."
+  defaultBio: string = "Description of you...";
 
   constructor(private readonly router: Router) {}
 
