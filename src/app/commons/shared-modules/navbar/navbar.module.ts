5d88f1e421cfd5c1df9cf9433579684b8be02b5a
huydang208 <huydang191@gmail.com>
2021-11-04 17:53:42 +0700

    create navbar - HuyDA

--- src/app/commons/shared-modules/navbar/navbar.module.ts
+++ src/app/commons/shared-modules/navbar/navbar.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
 import { RouterModule } from '@angular/router';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [NavbarComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    NgbNavModule
+  ],
   exports: [NavbarComponent],
 })
 export class NavbarModule {}
