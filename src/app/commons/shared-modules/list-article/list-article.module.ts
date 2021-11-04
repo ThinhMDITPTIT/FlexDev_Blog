3584fb2f236631bf5654bd04b0f9533f6f14ab9c
ThinhMD <ThinhMDITPTIT@gmail.com>
2021-11-04 17:58:16 +0700

    Build UI for Home module - ThinhMD

--- src/app/commons/shared-modules/list-article/list-article.module.ts
+++ src/app/commons/shared-modules/list-article/list-article.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListArticleComponent } from './list-article.component';
+import { ListArticleComponent } from './list-article/list-article.component';
+import { SingleArticleComponent } from './single-article/single-article.component';
 
 @NgModule({
-  declarations: [ListArticleComponent],
+  declarations: [SingleArticleComponent, ListArticleComponent],
   imports: [CommonModule],
   exports: [ListArticleComponent],
 })
--- src/app/feature-modules/home/home.module.ts
+++ src/app/feature-modules/home/home.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ListArticleModule } from 'src/app/commons/shared-modules/list-article/list-article.module';
+import { TagComponent } from './tag/tag.component';
+import { PaginationModule } from 'src/app/commons/shared-modules/pagination/pagination.module';
 
 @NgModule({
-  declarations: [HomepageComponent],
-  imports: [CommonModule, HomeRoutingModule, ListArticleModule],
+  declarations: [HomepageComponent, TagComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    ListArticleModule,
+    PaginationModule,
+  ],
   exports: [HomepageComponent],
 })
 export class HomeModule {}
