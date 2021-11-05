beb2b561bff2884c4b55e8663e1e8ab49ea9004d
huydang208 <huydang191@gmail.com>
2021-11-05 13:24:54 +0700

    update navbar - HuyDA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArticlesApiService } from './core/services/apis/articles-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'flexdev-blog';
+
+  constructor(private articleApiService: ArticlesApiService) {}
+
+  ngOnInit(){
+    this.articleApiService.getArticleByAuthor('johnjacob').subscribe(data => {
+      console.log(data);
+
+    })
+  }
 }
