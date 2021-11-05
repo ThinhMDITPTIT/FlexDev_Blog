ebb14ce18f5df9010e021a518e0a35fe993e404b
huydang208 <huydang191@gmail.com>
2021-11-06 01:15:52 +0700

    Pagination API - HuyDA

--- src/app/core/services/apis/articles-api.service.ts
+++ src/app/core/services/apis/articles-api.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { IArticle } from './../../../commons/models/IArticle';
 
 @Injectable({
   providedIn: 'root',
